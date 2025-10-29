b70f5bc28883420ed7aeeb97869eaa296c524e4a
Paul Biernat <biernp2@rpi.edu>
2025-10-29 11:12:28 -0400

    merge commit

--- ghidra_string_sniper_ext/README.md
+++
@@ -1,1 +0,0 @@
-# ghidra_string_sniper
--- ghidra_string_sniper_ext/linuxBuildnInstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -rf dist/
-gradle -PGHIDRA_INSTALL_DIR=/usr/local/bin/ghidraFiles
-rm -rf "$HOME/.config/ghidra/ghidra_11.4.2_PUBLIC/Extensions/ghidra_string_sniper_ext/"
-for file in "dist/*"; do
-	unzip -o $file -d "$HOME/.config/ghidra/ghidra_11.4.2_PUBLIC/Extensions/"
-done
