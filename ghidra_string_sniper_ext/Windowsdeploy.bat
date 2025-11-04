e963fe5da40d3d2acf2c54c68e13d944699e684e
CoryTsang <cory.tsang2@gmail.com>
2025-11-04 13:03:23 -0500

    Update Windowsdeploy.bat
    Minor fix to windows batch script and better instructions for developers to quickly build gradle and extract the plugin zip file to Ghidra's extension folder.

--- ghidra_string_sniper_ext/Windowsdeploy.bat
+++ ghidra_string_sniper_ext/Windowsdeploy.bat
@@ -2,20 +2,24 @@
 setlocal
 
 REM === Personal CONFIG PATHS ===
-:: Path to get to the ghidra_string_sniper directory.  Example: C:\Users\RPI\Documents\GitHub\ghidra-string-sniper\ghidra_string_sniper_ext
-set "DIST=ENTER.DATA.HERE"
+:: Path to get to the ghidra_string_sniper directory.  Example: C:\Users\RPI\Documents\GitHub\ghidra-string-sniper\ghidra_string_sniper_ext\dist
+:: If you do not have a \dist folder inside ghidra_string_sniper, make one before running this script and make sure to include the \dist folder at the end of your path below.
+set "DIST=ENTER PATH HERE"
 :: Path to get to your Ghidra's extensions folder.  Example: C:\Users\RPI\AppData\Roaming\ghidra\ghidra_11.4.2_PUBLIC\Extensions
-set "GHIDRA_EXT_PARENT=ENTER.DATA.HERE "
+set "GHIDRA_EXT_PARENT=ENTER PATH HERE"
 :: Path to get to your folder that contains your ghidraRun.bat file.  Example: C:\Users\RPI\PC\Desktop\ghidra_11.4.2_PUBLIC"
-set "GHIDRA_INSTALL=ENTER.DATA.HERE"
+set "GHIDRA_INSTALL=ENTER PATH HERE"
 
-echo NOTE Pt1: When launching Ghidra for the first time with this extension, you will need to enable the plugin when prompted.  
-echo NOTE Pt2: If no prompt is provided, you must go to file, configure, miscellaneous configure, and then check off the plugin.
+:: NOTE Pt1: When launching Ghidra for the first time with this extension, you will need to enable the plugin when prompted.  
+:: NOTE Pt2: If no prompt is provided, you must go to file, configure, miscellaneous configure, and then check off the plugin.
 
 echo Step 1: Delete dist folder contents
 if exist "%DIST%" (
     rmdir /S /Q "%DIST%"
 )
+mkdir "%DIST%"
+
+
 
 echo Step 2: Run Gradle build
 :: Will make dist folder in your ghidra string sniper directory.
