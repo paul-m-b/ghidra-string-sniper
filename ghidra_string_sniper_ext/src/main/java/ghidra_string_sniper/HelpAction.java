f7d5dec14386c2aad1ce48d696b59b37f34f00d3
CoryTsang <cory.tsang2@gmail.com>
2025-11-14 20:47:08 -0500

    Update HelpAction.java
    Completely refactored the structure to work on JFrame instead of JTabbedPane.
    
    Visually much cleaner and easier for users to read and select which tab to access for more information.
    
    Still needs a few changes: Updating information as the project progresses, more general UI changes, adding hyperlinks.

--- ghidra_string_sniper_ext/src/main/java/ghidra_string_sniper/HelpAction.java
+++ ghidra_string_sniper_ext/src/main/java/ghidra_string_sniper/HelpAction.java
@@ -21,90 +21,114 @@ public class HelpAction extends DockingAction {
 		this.provider = provider;
 
 		setToolBarData(new ToolBarData(Icons.HELP_ICON));
-		setDescription("Help and Tutorial");
+		setDescription("Help, Tutorial, and Misc.");
 	}
 
+	private JTabbedPane tabbedPane;
+	
 	@Override
 	public void actionPerformed(ActionContext context) {
-		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane = new JTabbedPane();
+		//TODO: Add roles when project is in final steps.  Update as more features are added.
 
-		//Tutorial section
-		JPanel tutorialPanel = new JPanel(new BorderLayout());
-		JTextArea tutorialText = new JTextArea("Welcome to Ghidra String Sniper!\n\n"
-		+ "This plugin helps you analyze strings in binaries efficiently.\n\n"
-		+ "Basic Usage:\n"
-		+ "1. Press the Refresh icon to search for strings within your current binary file.\n"
-		+ "2. View the extracted strings found in the table.\n"
-		+ "3. Double click on a string to be taken to the Results tab to see our LLM analysis as well as the repos this string appears inside of.\n\n"
-		//Change ^^ to double click or press deep research button once our prioritization and python scripts are integrated.
-		+ "Tip: You can click the ascending/descending button to sort by length.  "
-		);
-		tutorialText.setEditable(false);
-		tutorialText.setLineWrap(true);
-		tutorialText.setWrapStyleWord(true);
-		tutorialText.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		tutorialPanel.add(new JScrollPane(tutorialText), BorderLayout.CENTER);
-		
-		//Features section
-		JPanel featuresPanel = new JPanel(new BorderLayout());
-		JTextArea featuresText = new JTextArea("Included features:\n\n"
-			+ "• Search for strings — Searches for all strings within your current binary file.\n"
-			+ "• Sort Strings — Sorts strings by length (Ascending by default).\n"
-			+ "• Search Strings — Filters strings input and returns results to the table.\n"
-			+ "• Remove Strings — Removes the selected string from the table.\n"
-			+ "• Double clicking on a String — Performs analysis on the selected string, producing result to results tab.\n"
-			// ^^ Subject to change if we eventually make dedicated button for analysis			
-			+ "• Double clicking on an Address — Takes user to address location within Ghidra.\n"
-			//Will include this feature later + "• Export — Saves results to file.\n"
-			+ "• Help — Displays this help window."
-		);
-		featuresText.setEditable(false);
-		featuresText.setLineWrap(true);
-		featuresText.setWrapStyleWord(true);
-		featuresText.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		featuresPanel.add(new JScrollPane(featuresText), BorderLayout.CENTER);
-
-		
-		//Credits
-		JPanel creditsPanel = new JPanel(new BorderLayout());
-		JTextArea creditsText = new JTextArea("Credits section:\n\n"
-		+ "Ghidra :https://github.com/NationalSecurityAgency/ghidra\n"
-		//Turn this into a hyperlink^^
-		+ "Contributors:\n Paul Biernat '27 (Projet Lead)\n"
-		+ "Jack J '28 (Role Here)\n"
-		+ "Jack A '28 (Role Here)\n"
-		+ "Cory Tsang '26 (Role Here)"
-		//Change roles later
-		);
-		creditsText.setEditable(false);
-		creditsText.setLineWrap(true);
-		creditsText.setWrapStyleWord(true);
-		creditsText.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		creditsPanel.add(new JScrollPane(creditsText), BorderLayout.CENTER);
-		
-
-		//Useful Links
-		JPanel linksPanel = new JPanel(new BorderLayout());
-		JTextArea linksText = new JTextArea("Useful links section:\n\n"
-		+ "Ghidra String Sniper Repository: https://github.com/paul-m-b/ghidra-string-sniper\n"
-		//Should I add: "Ghidra :https://github.com/NationalSecurityAgency/ghidra\n"? I already have it in credits but I feel like it should go here as well.
-		//Turn this into a hyperlink^^
-		);
-		linksText.setEditable(false);
-		linksText.setLineWrap(true);
-		linksText.setWrapStyleWord(true);
-		linksText.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		linksPanel.add(new JScrollPane(linksText), BorderLayout.CENTER);
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				JFrame window = new JFrame("Help, Tutorial, and Misc.");
+				
+				window.setSize(800,450);
+				window.setLocationRelativeTo(null);
+				JTabbedPane tabPanel = new JTabbedPane();
+				
+				//Tutorial tab:
+				JPanel tutorialPage = new JPanel(new BorderLayout());
+				tutorialPage.add(new JLabel("Tutorial page here!"));
+				tabPanel.addTab("Tutorial", tutorialPage);
+				JTextArea tutorialText = new JTextArea("Welcome to Ghidra String Sniper!\n\n"
+				+ "This plugin helps you analyze strings in binaries efficiently.\n\n"
+				+ "Basic Usage:\n"
+				+ "1. Press the Refresh icon to search for strings within your current binary file.\n"
+				+ "2. View the extracted strings found in the table.\n"
+				+ "3. Double click on a string to be taken to the Results tab to see our LLM analysis as well as the repos this string appears inside of.\n\n"
+				//Change ^^ to double click or press deep research button once our prioritization and python scripts are integrated.
+				+ "Tip: You can click the ascending/descending button to sort by length.");
+				tutorialText.setLineWrap(true);
+				tutorialText.setWrapStyleWord(true);
+				tutorialText.setEditable(false);
+				JScrollPane tutorialScrollPane = new JScrollPane(tutorialText);
+				tutorialPage.add(tutorialScrollPane, BorderLayout.CENTER);
 
 
-		// Adding tabs
-		tabbedPane.addTab("Tutorial",tutorialPanel);
-		tabbedPane.addTab("Tutorial",featuresPanel);
-		tabbedPane.addTab("Credits",creditsPanel);
-		tabbedPane.addTab("Useful links",linksPanel);
 
-		JOptionPane.showMessageDialog(null,tabbedPane,	"String Sniper - Help & Tutorial", JOptionPane.INFORMATION_MESSAGE);
-		//TODO: Fix panel ui.  Looks scrunched up. Add roles when project is in final steps.  Update as more features are added.
+				//Features section
+				JPanel featuresPage = new JPanel(new BorderLayout());
+				featuresPage.add(new JLabel("Features page here!"));
+				tabPanel.addTab("Features",featuresPage);
+				JTextArea featuresText = new JTextArea("Included features:\n\n"
+					+ "• Search for strings — Searches for all strings within your current binary file.\n"
+					+ "• Sort Strings — Sorts strings by length (Ascending by default).\n"
+					+ "• Search Strings — Filters strings input and returns results to the table.\n"
+					+ "• Remove Strings — Removes the selected string from the table.\n"
+					+ "• Double clicking on a String — Performs analysis on the selected string, producing result to results tab.\n"
+					// ^^ Subject to change if we eventually make dedicated button for analysis			
+					+ "• Double clicking on an Address — Takes user to address location within Ghidra.\n"
+					//Will include this feature later + "• Export — Saves results to file.\n"
+					+ "• Help — Displays this help window."
+				);
+				featuresText.setEditable(false);
+				featuresText.setLineWrap(true);
+				featuresText.setWrapStyleWord(true);
+				JScrollPane featuresScrollPane = new JScrollPane(featuresText);
+				featuresPage.add(featuresScrollPane, BorderLayout.CENTER);
+
+
+				//Credits
+				JPanel creditsPage = new JPanel(new BorderLayout());
+				creditsPage.add(new JLabel("Credits page here!"));
+				tabPanel.addTab("Credits",creditsPage);
+				JTextArea creditsText = new JTextArea("Credits section:\n\n"
+				+ "Ghidra :https://github.com/NationalSecurityAgency/ghidra\n"
+				//Turn this into a hyperlink^^
+				+ "Contributors:\nPaul Biernat '27 (Projet Lead)\n" 
+				+ "Jack J '28 (Role Here)\n"
+				+ "Jack A '28 (Role Here)\n"
+				+ "Cory Tsang '26 (Role Here)"
+				//Change roles later
+				//Make it hyperlink to our LinkedIns
+				);
+				creditsText.setEditable(false);
+				creditsText.setLineWrap(true);
+				creditsText.setWrapStyleWord(true);
+				JScrollPane creditsScrollPane = new JScrollPane(creditsText);
+				creditsPage.add(creditsScrollPane, BorderLayout.CENTER);
+
+
+				//Useful Links
+				JPanel linksPage = new JPanel(new BorderLayout());
+				linksPage.add(new JLabel("Links page here!"));
+				tabPanel.addTab("Links",linksPage);
+				JTextArea linksText = new JTextArea("Useful links section:\n\n"
+				+ "Ghidra String Sniper Repository: https://github.com/paul-m-b/ghidra-string-sniper\n"
+				//Should I add: "Ghidra :https://github.com/NationalSecurityAgency/ghidra\n"? I already have it in credits but I feel like it should go here as well.
+				//Turn this into a hyperlink^^
+				);
+				linksText.setEditable(false);
+				linksText.setLineWrap(true);
+				linksText.setWrapStyleWord(true);
+				JScrollPane linksScrollPane = new JScrollPane(linksText);
+				linksPage.add(linksScrollPane, BorderLayout.CENTER);
+
+
+				//Adding tabs to the tab panel.  Can be increased for later additions
+				tabPanel.addTab("Tutorial",tutorialPage);
+				tabPanel.addTab("Features",featuresPage);
+				tabPanel.addTab("Credits",creditsPage);
+				tabPanel.addTab("Links",linksPage);
+
+
+				window.add(tabPanel);
+				window.setVisible(true);
+			}
+		});
 	}
 
 	@Override
