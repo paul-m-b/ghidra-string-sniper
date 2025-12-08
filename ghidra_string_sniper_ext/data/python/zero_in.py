a4687295fc556fa45467904543bd3a146868ebda
J-Angel <jack.angelichio@gmail.com>
2025-12-08 02:07:28 -0500

    Update zero_in.py
    Continued implementing "zero in" feature.

--- ghidra_string_sniper_ext/data/python/zero_in.py
+++ ghidra_string_sniper_ext/data/python/zero_in.py
@@ -1,6 +1,7 @@
 from function_match import FUNCTION_MATCH
 import logging
 import json
+import subprocess
 import os
 
 logging.basicConfig(level=logging.INFO)
@@ -11,7 +12,7 @@ class ZERO_IN():
         self.CONFIDENCE_THRESHOLD = 8
 
     '''
-    Goes through final GSS_results/MATCHES.json to find matches with confidence >= X
+    Goes through final GSS_results/MATCHES.json to find unique matches with confidence >= X
     '''
     def find_repos(self):
         with open('GSS_results/MATCHES.json', 'r') as f:
@@ -19,15 +20,56 @@ class ZERO_IN():
 
         repos = set()
         for entry in data.values():
+            confidence = entry[1]
+            if (confidence < self.CONFIDENCE_THRESHOLD):
+                continue
+
             file_path = entry[0]
             
             filename = os.path.basename(file_path)
 
+            repo_name = filename.split('_')[0]
+            repos.add(repo_name)
+
+        for repo_name in repos:
+            self.download_repos(self, "TODO: get org", repo_name)
+
     '''
     Downloads repo with string hit from GitHub
     '''
-    def download_repos(self):
-        pass
+    def download_repo(self, org: str, repo_name: str):
+        os.makedirs("GSS_zeroin", exist_ok=True)
+
+        # Build GitHub URL
+        if org:
+            repo_url = f"https://github.com/{github_org}/{repo_name}.git"
+            target_dir = os.path.join("GSS_zeroin", f"{org}_{repo_name}")
+        elif '/' in repo_name:
+            # repo_name already includes org
+            repo_url = f"https://github.com/{repo_name}.git"
+            org, repo = repo_name.split('/')
+            target_dir = os.path.join(output_dir, f"{org}_{repo}")
+        else:
+            logging.critical(f"Warning: No org/name specified for {repo_name}. Skipping.")
+            return None
+        
+        try:
+            logging.info(f"Downloading {repo_name}...")
+
+            result = subprocess.run(
+            ["git", "clone", repo_url, target_dir],
+            capture_output=True,
+            text=True,
+            timeout=120
+        )
+
+        except subprocess.TimeoutExpired:
+            logging.critical(f"Timedout while downloading {repo_name}")
+            return None
+        
+        except Exception as e:
+            logging.critical(f"Error downloading {repo_name}: {str(e)}")
+            return None
 
     '''
     Procures list of functions from opensource repo
